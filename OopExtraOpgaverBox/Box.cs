710fb526465d61190429d6611b1f168937ed449b
Dikshya-2 <82929581+Dikshya-2@users.noreply.github.com>
2022-04-15 00:03:21 +0200

    Add project files.

---
+++ OopExtraOpgaverBox/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopExtraOpgaverBox
+{
+    enum BoxType
+    {
+        lilleBox,
+        mellemBox,
+        storbox,
+    };
+    class Box : IComparable<Box>
+    {
+        public double Højde { get; set; }
+        public double Bredde { get; set; }
+        public double Længde { get; set; }
+        public BoxType BoxTypeT { get; set; }
+        public double Volume { get; set; }
+
+        public Box()
+        {
+
+        }
+
+        public Box(double højde, double bredde, double længde, BoxType boxtype)
+        {
+           Højde = højde;
+           Bredde =bredde  ;
+           Længde= længde;
+           BoxTypeT = boxtype;
+
+        }
+
+        public override string ToString() // (string lilleBoxNavn, string mellemBoxNavn, string storboxNavn)
+
+        {
+            return BoxTypeT.ToString();
+
+        }
+
+        public int CompareTo(Box other)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+        public static Box operator +(Box b1, Box b2)
+        {
+            Box resBox = new Box();
+            if (b1.BoxTypeT == BoxType.lilleBox && b2.BoxTypeT == BoxType.lilleBox)
+                resBox.BoxTypeT = BoxType.mellemBox;
+            else
+                resBox.BoxTypeT = BoxType.storbox;
+
+
+            resBox.Længde = b1.Længde + b2.Længde;
+            resBox.Bredde = b1.Bredde + b2.Bredde;
+            resBox.Højde = b1.Højde + b2.Højde;
+            return resBox;
+        }
+        public double volume()
+        {
+             Volume = Længde * Bredde * Højde;
+            return Volume;
+        }
+        public int compareTo(Box other)
+        {
+            return this.volume().CompareTo(other.volume());
+        }
+    }
+}
---
+++ OopExtraOpgaverBox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OopExtraOpgaverBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Opret i Main flere objekter af Box klassen,
+            //  vis brugen af begge constructors, alle properties – læg 2 boxe sammen og udskriv alle objekterne vha. ToString()//
+            Box b1 = new Box(5, 5, 5, BoxType.lilleBox);
+            Box b2 = new Box(10, 10, 10, BoxType.mellemBox);
+             
+
+            Box resBox = new Box();
+            resBox = b1 + b2;
+
+            Console.WriteLine (b1.ToString ());
+            Console.WriteLine (b2.ToString());
+            Console.WriteLine(resBox.ToString());
+        }
+    }
+}
